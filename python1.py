b97a39611509499adbaa27892427f2574efd7a40
Alex121q <54131552+Alex121q@users.noreply.github.com>
2019-10-14 16:48:07 +0800

    冒泡排序
    冒泡排序升序

--- python1.py
+++ python1.py
@@ -1,1 +1,7 @@
 print('hello world')
+list01 = [9, 5, 8, 3, 99, 1, 23, 66]
+for i in range(len(list01)-1):
+    for j in range(len(list01)-i-1):
+        if list01[j] > list01[j+1]:
+            list01[j], list01[j+1] = list01[j+1], list01[j]
+print(list01)
